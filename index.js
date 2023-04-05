4c45233417291b55b4ba8dfb4b28f9c66c8bdccb
Tu Nombre <you@example.com>
2023-04-04 22:14:00 -0400

    clase 4

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('funcionando')
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+const cors = require('cors')
+app.use(cors())
+app.get('/test',(request,response)=>{
+    response.send('testeando')
+})
+
+app.listen(3000,()=>{
+    console.log('Escuchando en el puerto 3000')
+})
