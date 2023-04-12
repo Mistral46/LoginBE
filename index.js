82c20127110c3e5df9d5b355425ee645f5300cf6
Tu Nombre <you@example.com>
2023-04-11 22:05:56 -0400

    clase 6

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const es6Renderer = require('express-es6-template-engine')
+const cors = require('cors')
 app.use(express.json())
 app.use(express.urlencoded())
-const cors = require('cors')
 app.use(cors())
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+app.set('view','views')
+app.set('view engine','html')
+app.engine('html', es6Renderer)
 
 app.get('/test',(request,response)=>{
     response.send('testeando')
