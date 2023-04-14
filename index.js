91ebfa4953aba41b7fdd616a2fc84ad48f304f0b
Tu Nombre <you@example.com>
2023-04-13 21:58:56 -0400

    validacion con openia

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ es6Renderer = require('express-es6-template-engine'),
 app.engine('html', es6Renderer);
 app.set('views', 'views');
 app.set('view engine', 'html');
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+
 app.get('/test',(request,response)=>{
     response.send('testeando')
 })
@@ -17,6 +24,26 @@ app.get('/test',(request,response)=>{
 app.post('/postData',(request,response) =>{
     console.log(request.body)
     response.send(`Los datos enviados fueron ${request.body.email} ${request.body.password}`)
+})
+
+app.get('/index',(request,response) =>{
+    response.render('index')
+})
+app.post('/enviarDatos',async (request,response) =>{
+    pregunta = `contiene lenguaje 
+     javascript en la siguiente expresion?
+    "${request.body.buscar}". Responde true o false en minusculas`
+    console.log(pregunta);
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: pregunta
+      });
+      console.log(completion.data.choices[0].text);
+    if(completion.data.choices[0].text.trim() == "true"){
+        response.render('recibe_datos',{locals: {busqueda : "Busqueda invalida"}})
+    }else{
+        response.render('recibe_datos',{locals: {busqueda : request.body.buscar}})
+    }  
 })
 
 app.listen(3000,()=>{
